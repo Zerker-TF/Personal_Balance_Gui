8dd0758450e8c74747eb240144f17b5df5b6c17a
Zerker <zerkerthethird@gmail.com>
2024-08-01 19:03:36 -0300

    window frames

---
+++ test/frames.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import ttk
+from widgets import create_widgets
+from widgets import create_table
+from widgets import create_graphs
+
+def create_frames(root):
+    style = ttk.Style(root)
+    root.tk.call("source","forest-light.tcl")
+    root.tk.call("source", "forest-dark.tcl")
+    style.theme_use("forest-dark")
+    #main frame
+    frame = tk.Frame(root)
+    frame.pack()
+    
+    #Ingresar datos frame
+    widgets_frame = ttk.LabelFrame(frame,text="Ingresar Datos")
+    widgets_frame.grid(row=0, column=0, padx=20, pady=20)
+
+    
+    #Treeview table frame
+    treeframe = ttk.Frame(frame)
+    treeframe.grid(row=0,column=1,padx=30,pady=5)
+ 
+    
+    #Graphs frame
+    graphframe = ttk.Frame(frame)
+    graphframe.grid(row=1,column=0,columnspan=2)
+    create_widgets(widgets_frame)
+    create_table(treeframe,graphframe,frame)
