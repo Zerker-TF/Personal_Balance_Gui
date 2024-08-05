8837e59f7794d2e28a455c822a7997fbd94904f4
Zerker <zerkerthethird@gmail.com>
2024-08-05 15:56:44 -0300

    added chart refresh

--- Data_Saving.py
+++ Data_Saving.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
 from tkinter import messagebox
+from tkinter import simpledialog
 import openpyxl
+from datetime import datetime
 from datetime import date
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
@@ -16,70 +18,93 @@ def create_savings_window(root):
         ahorros = []
         for row in sheet.iter_rows(min_row=2, values_only=True):
            if row[0] not in ahorros:
-            ahorros.append(row[0])
+                ahorros.append(row[0])
         
         return ahorros
     
     def new_ahorro():
+        
+        if ahorro_select.get() == "Nuevo Ahorro":
+            new_ahorro_name = simpledialog.askstring("Nuevo Ahorro","Ingrese el nombre y ingrese los valores")
+            if new_ahorro_name is not None:
+                ahorro_select.set(new_ahorro_name)
+                update_combobox()
+                ahorro_amount.focus_set()
+            else:
+                messagebox.showerror("Error","Debe ingresar un nombre para continuar.")
+            return
+        
         filepath = "./Gastos.xlsx"
         workbook = openpyxl.load_workbook(filepath)
-        #print(selected_month)
         sheet = workbook["Ahorros"]
         ahorro_name = ahorro_select.get()
-        amount_added = float(ahorro_amount.get())
-        objective = float(ahorro_final.get())
-        for row in sheet.iter_rows(min_row=2,values_only=True):
+        try:
+            amount_added = float(ahorro_amount.get())
+            objective = float(ahorro_final.get())
+        except ValueError:
+            messagebox.showerror("Error", "Debe ingresar valores numéricos válidos.")
+            return
+        
+        for row in sheet.iter_rows(min_row=2, values_only=True):
             if row[0] == ahorro_name:
                 total_amount = row[1] + amount_added
-                sheet.append([ahorro_name,total_amount,objective,date.today(),amount_added])
+                sheet.append([ahorro_name, total_amount, objective, date.today(), amount_added])
                 break
         else:
-            sheet.append([ahorro_name,amount_added,objective,date.today(),amount_added])
+            sheet.append([ahorro_name, amount_added, objective, date.today(), amount_added])
         workbook.save(filepath)
+        
         update_combobox()
         update_charts()
         
     def update_combobox():
+        ahorros = ahorros_list()
         ahorro_select['values']=["Nuevo Ahorro"] + ahorros_list()
-    
+        ahorro_table['values'] = ahorros
+        
     def get_values():
 
         filepath = "./Gastos.xlsx"
         workbook = openpyxl.load_workbook(filepath)
         sheet = workbook["Ahorros"]
-
         ahorros = []
         amounts_saved = []
         objectives = []
         dates = []
-        #amounts_added = []
-
+        
         for row in sheet.iter_rows(min_row=2, values_only=True):
-           if row[0] not in ahorros: 
-             ahorros.append(row[0])
-             amounts_saved.append(row[1])
-             objectives.append(row[2])
-             dates.append(row[3])
-            # amounts_added.append(row[4])
-        # Sort dates
-        sorted_data = sorted(zip(ahorros,amounts_saved,objectives,dates), key=lambda x: x[3], reverse=True)    
-        latest_data = []        
+            if row[0] not in ahorros: 
+                ahorros.append(row[0])
+                amounts_saved.append(row[1])
+                objectives.append(row[2])
+                
+                # Convertir todas las fechas a objetos datetime
+                if isinstance(row[3], str):
+                    try:
+                        dates.append(datetime.strptime(row[3], "%Y-%m-%d"))
+                    except ValueError:
+                        dates.append(datetime.strptime(row[3],"%d/%m/%Y"))
+                else:
+                    dates.append(row[3])
+        
+        sorted_data = sorted(zip(ahorros, amounts_saved, objectives, dates), key=lambda x: x[3], reverse=True)
+        latest_data = []
         for ahorro, _, _, _ in sorted_data:
             if ahorro not in [x[0] for x in latest_data]:
-                latest_data.append(next((x for x in sorted_data if x[0]==ahorro),None))
-        
-        ahorros,amounts_saved,objectives,dates = zip(*latest_data)
-        
-        return ahorros, amounts_saved, objectives,dates
+                latest_data.append(next((x for x in sorted_data if x[0] == ahorro), None))
+                ahorros, amounts_saved, objectives, dates = zip(*latest_data)
+        return ahorros, amounts_saved, objectives, dates
 
-    def update_charts():
+    def clear_charts():
+        for widget in visual_frame.winfo_children():
+            widget.destroy()
     
+    
+    def update_charts():
+        clear_charts()
         ahorros, amounts_saved, objectives,dates = get_values()
 
-
         fig, axs = plt.subplots(len(ahorros), 1, figsize=(8, 1.1*len(ahorros)))
-
-
         fig.suptitle('Listado de Ahorros', color="#bfbfbf")
 
 
@@ -108,22 +133,19 @@ def create_savings_window(root):
         fig.patch.set_facecolor("#313131")
 
         canvas = FigureCanvasTkAgg(fig, master=visual_frame)
-        canvas.draw()
-        
-              
+        canvas.draw()     
         canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
      
     def insert_table():
 
         selected_item = ahorro_table.get()
-
+        #Clear table
         for item in treeview.get_children():
             treeview.delete(item)
-
         filepath = "./Gastos.xlsx"
         workbook = openpyxl.load_workbook(filepath)
         sheet = workbook["Ahorros"]
-       
+            
         for row in sheet.iter_rows(min_row=2, values_only=True):
             if row[0] == selected_item:
                 treeview.insert("","end",values=(row[3],row[1],row[2]))
@@ -168,21 +190,21 @@ def create_savings_window(root):
     widgets_frame2 = ttk.LabelFrame(widgets_frame1,text="Retirar")
     widgets_frame2.grid(row=5,column=0)
     
-    ahorro_select = ttk.Combobox(widgets_frame2,values=lista_ahorros)
-    ahorro_select.insert(0,"Seleccionar ahorro")
+    ahorro_select2 = ttk.Combobox(widgets_frame2,values=lista_ahorros)
+    ahorro_select2.insert(0,"Seleccionar ahorro")
     #ahorro_select.current(0)
-    ahorro_select.grid(row=0,column=0,padx=5,pady=5,sticky="ew")     
+    ahorro_select2.grid(row=0,column=0,padx=5,pady=5,sticky="ew")     
     
-    ahorro_amount = ttk.Entry(widgets_frame2)
-    ahorro_amount.insert(0,"Ingrese el monto a retirar")
-    ahorro_amount.bind("<FocusIn>",lambda e: ahorro_amount.delete('0','end'))
-    ahorro_amount.grid(row=1,column=0,padx=5,pady=5,sticky="ew")
+    ahorro_amount2 = ttk.Entry(widgets_frame2)
+    ahorro_amount2.insert(0,"Ingrese el monto a retirar")
+    ahorro_amount2.bind("<FocusIn>",lambda e: ahorro_amount.delete('0','end'))
+    ahorro_amount2.grid(row=1,column=0,padx=5,pady=5,sticky="ew")
     
     savings_retirar = ttk.Button(widgets_frame2, text="Retirar")
     savings_retirar.grid(row=3,column=0,padx=5,pady=5,sticky="ew")
     
-    separator = ttk.Separator(widgets_frame2)
-    separator.grid(row=4, column=0, padx=(20,10), pady=10, sticky="ew")
+    separator2 = ttk.Separator(widgets_frame2)
+    separator2.grid(row=4, column=0, padx=(20,10), pady=10, sticky="ew")
  
     
     visual_frame = ttk.Frame(frame)
