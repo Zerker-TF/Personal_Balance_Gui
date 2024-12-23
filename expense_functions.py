e0da937f7b3926e6244f3e9c694a1db6843526fe
Zerker <zerkerthethird@gmail.com>
2024-12-23 15:21:27 -0300

    fixed data not loading from database due to months being strings instead of int

--- expense_functions.py
+++ expense_functions.py
@@ -2,46 +2,13 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 import os
 import sqlite3
-#import openpyxl, openpyxl.workbook
 from tkcalendar import Calendar, DateEntry
 import datetime
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import pyperclip
 
-#def load_data(month,year, treeview,graphs):
-#  
-#    filepath = "./Gastos.xlsx"
-#    
-#   
-#    workbook = openpyxl.load_workbook(filepath)
-#    
-#    try:
-#        sheet = workbook[month]
-#    except KeyError:
-#        sheet = workbook.active
-#    
-#    list_values = [list(row) for row in sheet.values]
-#    # Filter the data based on the selected year
-#    list_values = [row for row in list_values[1:] if str(row[0]).split('-')[0] == year]
-#    
-#    #Sort by date
-#    for i, row in enumerate(list_values[1:]):
-#        if isinstance(row[0],str):
-#            list_values[i+1][0] = datetime.datetime.strptime(row[0], "%Y-%m-%d").date()
-#        elif isinstance (row[0], datetime.datetime):
-#            list_values[i+1][0] = row[0].date()
-#    
-#    list_values[1:] = sorted(list_values[1:], key=lambda x: x[0])
-#   
-#    for item in treeview.get_children():
-#        treeview.delete(item)
-#    
-#    for row in list_values[0:]:
-#        row = [" " if value is None else value for value in row]
-#        treeview.insert("","end",values=row[0:4])
-#    
-#    show_chart(True,month,year,graphs) 
+
 
 def load_data(month, year, treeview, graphs):
    conn = sqlite3.connect("./Gastos.db")
@@ -50,7 +17,7 @@ def load_data(month, year, treeview, graphs):
    query = '''
       SELECT fecha, categoria, monto, descripcion, cuotas
       FROM gastos
-      WHERE strftime('%Y', fecha) = ? AND strftime('m', fecha) = ?
+      WHERE strftime('%Y', fecha) = ? AND strftime('%m', fecha) = ?
    '''
    cursor.execute(query, (year, month))
    list_values = cursor.fetchall()
@@ -66,42 +33,6 @@ def load_data(month, year, treeview, graphs):
    show_chart(True,month,year,graphs)
    
 
-#def insert_data(exp_date,category,amount,cuota,desc,year_select,treeview,graphs):
-#    months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-#    
-#    date_obj = datetime.datetime.strptime(exp_date, "%m/%d/%y")
-#    date = date_obj.strftime("%Y-%m-%d")
-#    month = date[5:7]
-#    year = int(date[:4])
-# 
-#    filepath = "./Gastos.xlsx"
-#        
-#    workbook = openpyxl.load_workbook(filepath)
-#    
-#   # calculate cuotas
-#    if cuota != "1":
-#       cuota = int(cuota)
-#       divided_amount = amount / cuota
-#       month_index = int(month)
-#       for i in range(cuota):
-#          new_date = f"{year}-{str(month_index).zfill(2)}-{date[8:]}"
-#          new_row = [new_date, category, divided_amount, desc if desc else " ", cuota]
-#          sheet = workbook[months[month_index-1]]         
-#          sheet.append(new_row)
-#          # Move to the next month
-#          month_index += 1
-#          if month_index > 12:
-#             month_index = 1
-#             year += 1
-#    else:
-#       sheet = workbook[months[int(month)-1]]
-#       new_row = [date, category, amount, desc if desc else " ", cuota if cuota else 1]
-#       sheet.append(new_row)     
-#    workbook.save(filepath)
-#
-#    treeview.insert("","end",values=[date,category,amount,desc if desc else " "])
-#    month_select = months[int(month)-1]
-#    show_chart(True,month_select,year_select,graphs) 
 
 def insert_data(exp_date,category,amount,cuota,desc,year_select,treeview,graphs) :
    
@@ -139,11 +70,13 @@ def insert_data(exp_date,category,amount,cuota,desc,year_select,treeview,graphs)
                      ''',new_row)
 
    conn.commit()
-   
+   print("data guardada")
    
    treeview.insert("","end",values=[date,category,amount,desc if desc else " "])
    month_select = date_obj.strftime("%B")
    show_chart(True ,month_select,year_select,graphs)
+   
+   
    conn.close()
 
 #def copy_row(event,treeview):
@@ -235,6 +168,11 @@ def insert_data(exp_date,category,amount,cuota,desc,year_select,treeview,graphs)
 def show_chart(chart_shown,month_select,year,graphframe):
        global  canvas, pie_canvas
        
+       month_number = {
+          "Enero":"01", "Febrero":"02", "Marzo": "03","Abril":"04","Mayo":"05",
+          "Junio":"06", "Julio":"07","Agosto":"08","Septiembre":"09","Octubre":"10",
+          "Noviembre":"11","Diciembre":"12"}
+       
        colors = {
 
         "Comida": ['#4CAF50', '#2E865F'],
@@ -260,144 +198,129 @@ def show_chart(chart_shown,month_select,year,graphframe):
           }
     
        if chart_shown:
-            #####
+            
          conn = sqlite3.connect("./Gastos.db")
          cursor = conn.cursor()
          print("entra al if show chart")
+         
+         print(f"Año:{year}")
+         print(f"Mes:{month_select.zfill(2)}")
+         month_n = month_number.get(month_select,None)
          query = '''
             SELECT categoria, SUM(monto)
             FROM gastos
             WHERE strftime('%Y', fecha) =? AND strftime('%m',fecha) = ?
             GROUP BY categoria    
          '''
-       cursor.execute(query, (year, month_select))
-       data = cursor.fetchall()
+         print("Executing query:", query)
+         cursor.execute(query, (year, month_n))
+         data = cursor.fetchall()
+         print("Resultado de query:",data)
+         
+         print("cargo la data")
+         if not data:
+             return #nada que mostrar
+	     
+         
+         print("paso el if de no data")
+         # preparo la data
+         categorias = ["Comida", "Alquiler", "Expensas", "Internet", "Agua", "Gas", "Luz", "Transporte", "Salud", "Bolucompra"]       
+         amounts = [0] * len(categorias)
+         print("preparo las categorias")
+         for row in data:
+            category, total_amount = row
+            if category in categorias:
+               index = categorias.index(category)
+               amounts[index] = total_amount
+  
+         print("cargo las filas")
+         sorted_categories = [category for _, category in sorted(zip(amounts, categorias), reverse=True)]
+         sorted_amounts = [amount for amount, _ in sorted(zip(amounts, categorias), reverse=True)]
+         total = sum(amounts)
+         ingresos = sum(float(row[2]) for row in data if row[1] == "Ingresos")
+         saldo_restante = ingresos-total
+          
+         # Percentages for pie chart   
+         percentages = [f"{(amount/total)*100:.2f}%" for amount in amounts]
+  
+         # Create a new category "Otros" for pie chart
+         otros_amount = sum(amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) < 3)
+         otros_percentage = f"{(otros_amount/total)*100:.2f}%"
         
-       conn.close()
-       print("cerro la conexion con sql")
-       print(data)
-       if not data:
-           return #nada que mostrar
-        
-       # preparo la data
-       categorias = ["Comida", "Alquiler", "Expensas", "Internet", "Agua", "Gas", "Luz", "Transporte", "Salud", "Bolucompra"]       
-       amounts = [0] * len(categorias)
-       print("preparo las categorias")
-       for row in data:
-          category, total_amount = row
-          if category in categorias:
-             index = categorias.index(category)
-             amounts[index] = total_amount
-
-       print("cargo las filas")
-       sorted_categories = [category for _, category in sorted(zip(amounts, categorias), reverse=True)]
-       sorted_amounts = [amount for amount, _ in sorted(zip(amounts, categorias), reverse=True)]
-       total = sum(amounts)
-       ingresos = sum(float(row[2]) for row in data if row[1] == "Ingresos")
-       saldo_restante = ingresos-total
-        
-       # Percentages for pie chart   
-       percentages = [f"{(amount/total)*100:.2f}%" for amount in amounts]
-
-        # Create a new category "Otros" for pie chart
-       otros_amount = sum(amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) < 3)
-       otros_percentage = f"{(otros_amount/total)*100:.2f}%"
       
-    
-       if otros_amount > 0:
-         pie_categorias = [category for category, percentage in zip(categorias, percentages) if float(percentage.strip('%')) >= 3] + ["Otros"]
-         pie_amounts = [amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) >= 3] + [otros_amount]
-       else:
-         pie_categorias = [category for category, percentage in zip(categorias, percentages) if float(percentage.strip('%')) >= 3]
-         pie_amounts = [amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) >= 3] 
-
-
-        # Create the figure and axis
-       figure, ax = plt.subplots(figsize=(8, 6))
-        # Create the figure and axis for pie chart
-       figure2, ax2 = plt.subplots(figsize=(4.5, 6))
-
-        # Title and labels
-       figure.suptitle(f"Total gastado por categoria para el mes {month_select}", y=0.98)
-       ax.set_xlabel("Categoria")
-       ax.set_ylabel("Total ($ARS)")
-
-        # Plot the bar chart  
-       bars = ax.bar(sorted_categories, sorted_amounts, color=[colors[category][0] for category in sorted_categories], edgecolor='black', linewidth=1)
-       # Add shadows
-       ax.bar([x - 0.5 for x in range(len(sorted_categories))], sorted_amounts, color=[colors[category][1] for category in sorted_categories], edgecolor='black', linewidth=1, zorder=10)
-
-       ax.set_xticks([x - 0.25 for x in range(len(categorias))])
-       ax.set_xticklabels(sorted_categories, rotation=45, ha='right')
-       ax.set_facecolor('#c0c0c0')
-
-        # Prints the total spent in the selected month
-       ax.text(0.95, 0.95, f"Total: ${int(total):,}", ha="right", va="top", 
-           fontweight='bold', color='black', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
-        
-        # Prints the remaining budget for the month
-       if saldo_restante >= 0:
-            ax.text(0.95, 0.88, f"Saldo: ${int(saldo_restante):,}", ha="right", va="top", 
-               fontweight='bold', color='black', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
-       else:
-           ax.text(0.95, 0.88, f"Saldo: ${int(saldo_restante):,}", ha="right", va="top", 
-               fontweight='bold', color='red', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
-        
-       figure.patch.set_facecolor("#808080")
-       figure.tight_layout() # makes the labels fit the plot area
-
-        # Create the canvas for the bar chart
-       canvas = FigureCanvasTkAgg(figure, master=graphframe)
-       canvas.draw()
-       canvas.get_tk_widget().grid(row=0, column=0)
-        
-        # pctdistance moves the label x amount from the center to the outside
-       ax2.pie(pie_amounts, autopct=lambda p: '{:.1f}%'.format(p), startangle=90, pctdistance=0.75, radius=0.8,textprops={'fontsize':10, 'fontweight': 'bold'}  ,colors=['#4CAF50', '#FF9800', '#009688', '#2196F3', '#66c0f4', '#c7d5e0', '#ffd700', '#ff7f50', '#d11141', '#808080'], labels=pie_categorias)
-       ax2.axis('equal')
-       figure2.suptitle("Porcentaje por categoria", y=0.98)
-       figure2.patch.set_facecolor("#808080")
-       figure2.tight_layout()
-
-        # Add each category total to the top of each bar
-       for bar, amount in zip(bars.patches, sorted_amounts):
-          if int(amount) >= 1000:
-              text = f"{amount/1000:.1f}k"
-          else:
-              text= f"{int(amount):,}"
-          ax.text(bar.get_x() - 0.15 + bar.get_width()/2, bar.get_y() + bar.get_height(), text, ha='center', va='bottom', fontweight='bold', color='black',fontsize=9)
-
-        # Canvas for pie chart
-       pie_canvas = FigureCanvasTkAgg(figure2, master=graphframe)
-       pie_canvas.draw()
-       pie_canvas.get_tk_widget().grid(row=0, column=1)
-
-       print("creo los frames")
-        # Resize window when chart is shown
-       graphframe.grid_rowconfigure(0, weight=1)
-       graphframe.grid_columnconfigure(0, weight=1)    
+         if otros_amount > 0:
+           pie_categorias = [category for category, percentage in zip(categorias, percentages) if float(percentage.strip('%')) >= 3] + ["Otros"]
+           pie_amounts = [amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) >= 3] + [otros_amount]
+         else:
+           pie_categorias = [category for category, percentage in zip(categorias, percentages) if float(percentage.strip('%')) >= 3]
+           pie_amounts = [amount for amount, percentage in zip(amounts, percentages) if float(percentage.strip('%')) >= 3] 
+  
+  
+         # Create the figure and axis
+         figure, ax = plt.subplots(figsize=(8, 6))
+         # Create the figure and axis for pie chart
+         figure2, ax2 = plt.subplots(figsize=(4.5, 6))
+  
+         # Title and labels
+         figure.suptitle(f"Total gastado por categoria para el mes {month_select}", y=0.98)
+         ax.set_xlabel("Categoria")
+         ax.set_ylabel("Total ($ARS)")
+  
+         # Plot the bar chart  
+         bars = ax.bar(sorted_categories, sorted_amounts, color=[colors[category][0] for category in sorted_categories], edgecolor='black', linewidth=1)
+         # Add shadows
+         ax.bar([x - 0.5 for x in range(len(sorted_categories))], sorted_amounts, color=[colors[category][1] for category in sorted_categories], edgecolor='black', linewidth=1, zorder=10)
+  
+         ax.set_xticks([x - 0.25 for x in range(len(categorias))])
+         ax.set_xticklabels(sorted_categories, rotation=45, ha='right')
+         ax.set_facecolor('#c0c0c0')
+  
+         # Prints the total spent in the selected month
+         ax.text(0.95, 0.95, f"Total: ${int(total):,}", ha="right", va="top", 
+             fontweight='bold', color='black', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
+          
+         # Prints the remaining budget for the month
+         if saldo_restante >= 0:
+              ax.text(0.95, 0.88, f"Saldo: ${int(saldo_restante):,}", ha="right", va="top", 
+                 fontweight='bold', color='black', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
+         else:
+             ax.text(0.95, 0.88, f"Saldo: ${int(saldo_restante):,}", ha="right", va="top", 
+                 fontweight='bold', color='red', fontsize=10, transform=ax.transAxes, bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5'))
+          
+         figure.patch.set_facecolor("#808080")
+         figure.tight_layout() # makes the labels fit the plot area
+  
+         # Create the canvas for the bar chart
+         canvas = FigureCanvasTkAgg(figure, master=graphframe)
+         canvas.draw()
+         canvas.get_tk_widget().grid(row=0, column=0)
+          
+         # pctdistance moves the label x amount from the center to the outside
+         ax2.pie(pie_amounts, autopct=lambda p: '{:.1f}%'.format(p), startangle=90, pctdistance=0.75, radius=0.8,textprops={'fontsize':10, 'fontweight': 'bold'}  ,colors=['#4CAF50', '#FF9800', '#009688', '#2196F3', '#66c0f4', '#c7d5e0', '#ffd700', '#ff7f50', '#d11141', '#808080'], labels=pie_categorias)
+         ax2.axis('equal')
+         figure2.suptitle("Porcentaje por categoria", y=0.98)
+         figure2.patch.set_facecolor("#808080")
+         figure2.tight_layout()
+  
+         # Add each category total to the top of each bar
+         for bar, amount in zip(bars.patches, sorted_amounts):
+            if int(amount) >= 1000:
+                text = f"{amount/1000:.1f}k"
+            else:
+                text= f"{int(amount):,}"
+            ax.text(bar.get_x() - 0.15 + bar.get_width()/2, bar.get_y() + bar.get_height(), text, ha='center', va='bottom', fontweight='bold', color='black',fontsize=9)
+  
+         # Canvas for pie chart
+         pie_canvas = FigureCanvasTkAgg(figure2, master=graphframe)
+         pie_canvas.draw()
+         pie_canvas.get_tk_widget().grid(row=0, column=1)
+  
+         print("creo los frames")
+         # Resize window when chart is shown
+         graphframe.grid_rowconfigure(0, weight=1)
+         graphframe.grid_columnconfigure(0, weight=1)    
+         
+         conn.close()
+         print("cerro la coneccion")
      
-     #       
-        # Set the active sheet based on the selected month
-     #   filepath = "./Gastos.xlsx"
-     #   workbook = openpyxl.load_workbook(filepath)
-     #  
-     #   if isinstance(month_select,str):
-     #    selected_month = month_select
-     #   else:
-     #    selected_month = month_select.get()
-     #   #print(selected_month)
-     #   try:
-     #     sheet = workbook[selected_month]
-     #   except KeyError:
-     #     sheet = workbook.active
-     #   
-     #   if sheet.max_row < 2:
-     #      return
-     #   data = list(sheet.values)
-     #   # Filter the data based on the selected year
-     #
-     #   data = [row for row in data if str(row[0]).split('-')[0] == year]
-     #   categorias =["Comida","Alquiler","Expensas","Internet","Agua","Gas","Luz","Transporte","Salud","Bolucompra"]
-    #
-     #   
-     #   amounts = [sum(float(row[2]) for row in data if row[1] == category) for category in categorias]
+     
--- mainv2.py
+++ mainv2.py
@@ -2,41 +2,10 @@ import tkinter as tk
 from tkinter import ttk
 from Data_Expensesv2 import create_expense_window
 from Data_Savingv2 import create_savings_window
-#import openpyxl
 import sqlite3
 import os
 
-#def resize(event):
-#    selected_tab = event.widget.select()
-#    tab_text = event.widget.tab(selected_tab, "text")
-#
-#    if tab_text == "Gastos":
-#        root.minsize(1260, 410)
-#        root.maxsize(1260, 950)
-#       
-#    elif tab_text == "Ahorros":
-#        root.minsize(1260, 880)
-#        root.maxsize(1260, 945)
-#        root.wm_geometry("1260x900")
-#
-#def initialize_sheets():
-#    filepath = "./Gastos.xlsx"
-#    months = ["Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"]
-#    if not os.path.exists(filepath):
-#     workbook = openpyxl.Workbook()
-#     workbook.remove(workbook.active)
-#
-#     ahorros_sheet = workbook.create_sheet(title="Ahorros",index=0)
-#     ahorros_heading = ["Fecha","Nombre","Total","Objetivo","Monto agregado"]
-#     ahorros_sheet.append(ahorros_heading)
-#
-#
-#     for name in months:
-#         sheet = workbook.create_sheet(title=name)
-#         heading = ["Fecha","Categoria","Monto","Descripcion","Cuotas"]
-#         sheet.append(heading)
-#     workbook.save(filepath)
-#    
+
 def initialize_database():
     filepath = "./Gastos.db"
     
